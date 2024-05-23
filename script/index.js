6566df345708631f07d6fd8b2743e63aa2fef7a9
Akashsaini12 <akashsaini7456@gmail.com>
2024-05-23 15:23:51 +0530

    Done

--- script/index.js
+++ script/index.js
@@ -1,30 +1,26 @@
-// var slideImages = document.querySelectorAll("img");
-// var next = document.querySelector(".next");
-// var prev = document.querySelector(".prev");
+var filteredUser = userData.filter(function (ele) {
+  return ele.email === user.email;
+});
 
-// var counter = 0;
+console.log(filteredUser);
+document.querySelector("#loggedInUser").innerText = filteredUser[0].name;
+if (!user) {
+  window.location.href = "login.html";
+}
 
-// next.addEventListener('click', slideNext);
-// function slideNext() {
-//   slideImages[counter].style.animation = 'next1 0.5s ease-in forwords';
-//   if (counter >= slideImages.length - 1) {
-//     counter = 0;
-//   } else {
-//     counter++;
-//   }
-
-// }
 let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
--- script/snekers.js
+++ script/snekers.js
@@ -2,70 +2,70 @@ var productData = [
   {
     id: "1",
     description: "Jordan 12 Brilliant",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/e8AAAOSw8DZk6Nws/s-l225.webp",
     price: "499",
   },
   {
     id: "2",
     description: "Jordan 1 Retro",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/mQUAAOSwHbxk6Nwy/s-l225.webp",
     price: "499",
   },
   {
     id: "3",
     description: "Jordan 7 Chambray",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/9KAAAOSw0Gdk6Nw4/s-l225.webp",
     price: "499",
   },
   {
     id: "4",
-    description: "Jordan Luka 2 Low Space Hunter",
-    category: ["last release", "trending"],
+    description: "Jordan Luka 2 Low Space",
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/eGAAAOSwa31k6Nw9/s-l225.webp",
     price: "499",
   },
   {
     id: "5",
     description: "Jordan 1 Retro High OG",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/RrAAAOSwvJlk6NxD/s-l225.webp",
     price: "499",
   },
   {
     id: "6",
     description: "Jordan 4 Retro Mid White Oreo",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/WewAAOSwWdhk6NxW/s-l225.webp",
     price: "499",
   },
   {
     id: "7",
     description: "Jordan 3 Retro Mid White",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/kxUAAOSwt3tk6Nxc/s-l225.webp",
     price: "499",
   },
   {
     id: "8",
-    description: "Jordan 4 Retro Mid Military Black",
-    category: ["last release", "trending"],
+    description: "Jordan 4 Retro Mid Military",
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/vewAAOSwAcVk6Nxh/s-l225.webp",
     price: "499",
   },
   {
     id: "9",
     description: "Nike Dunk Low SB",
-    category: ["last release", "trending"],
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/NEsAAOSwYNtk6Nxn/s-l225.webp",
     price: "499",
   },
   {
     id: "10",
-    description: "Jordan 4 Retro Mid Midnight Navy",
-    category: ["last release", "trending"],
+    description: "Jordan 4 Retro Mid Midnight",
+    category: "trending",
     url: "https://i.ebayimg.com/thumbs/images/g/EBMAAOSwoyVk6Nxt/s-l225.webp",
     price: "499",
   },
@@ -74,80 +74,70 @@ var productData = [
 
   {
     id: "11",
-    description:
-      "adidas Originals Retropy F2 Blue White Orange GX4637 Men Size 8-13 New Trainer",
+    description: "adidas Originals Retropy F2",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/I6gAAOSwzH1k6Ny9/s-l225.webp",
     price: "599",
   },
   {
     id: "12",
-    description:
-      "adidas Originals Rivalry 86 Low The Simpsons Treehouse Of Horrors IE7180 New",
+    description: "adidas Originals Rivalry 86",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/mDsAAOSwlANk6NzD/s-l225.webp",
     price: "399",
   },
   {
     id: "13",
-    description:
-      "Adidas Originals Ozelia Shoes - Men's - Sand / Black / Orange - Sizes 9-11.5",
+    description: "Adidas Originals Ozelia",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/mSAAAOSwO2hk6NzJ/s-l225.webp",
     price: "899",
   },
   {
     id: "14",
-    description:
-      "New adidas Originals Court Mens Leather Low Top Athletic sneaker white all size",
+    description: "New adidas Originals Court",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/EYAAAOSwB7lk6NzO/s-l225.webp",
     price: "299",
   },
   {
     id: "15",
-    description:
-      "adidas Originals Marathon TR in Dark Blue and White Men's Trainers Limited Stock",
+    description: "adidas Originals Marathon TR",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/pfYAAOSwaQZk6NzU/s-l225.webp",
     price: "799",
   },
   {
     id: "16",
-    description:
-      "Adidas Originals Stan Smith Shoes Sneakers White Green M20324 Men's Size 19",
+    description: "Adidas Originals Stan Smith",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/nrMAAOSwBwVk6Nzb/s-l225.webp",
     price: "599",
   },
   {
     id: "17",
-    description:
-      "adidas Originals Retropy E5 Brown Yellow Orange GW6778 Men's Size 8-13 New",
+    description: "adidas Originals Retropy E5",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/qvUAAOSwr-Zk6Nzk/s-l225.webp",
     price: "499",
   },
   {
     id: "18",
-    description:
-      "Adidas Originals Ozmillen Men’s Sneaker Running Shoe White Athletic Trainer #447",
+    description: "Adidas Originals Ozmillen",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/SSIAAOSwLx1k6Nzp/s-l225.webp",
     price: "499",
   },
   {
     id: "19",
-    description:
-      "Adidas Originals Samba Vegan x Mickey Mouse White Sneakers GY1890 Men's Sz 10.5",
+    description: "Adidas Originals Samba Vegan",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/SZ4AAOSw7pdmCIbz/s-l960.webp",
     price: "599",
   },
   {
     id: "20",
-    description:
-      "IG0793 Korn adidas Originals Supermodified Cloud White Core Black Purple (Men's)",
+    description: "IG0793 Korn adidas Originals",
     category: "adidas",
     url: "https://i.ebayimg.com/thumbs/images/g/uNAAAOSwc25mJxkf/s-l960.webp",
     price: "999",
@@ -157,36 +147,35 @@ var productData = [
 
   {
     id: "21",
-    description: "Nike Air Max Alpha 5 Trainer ",
+    description: "Nike Air Max Alpha 5 Trainer",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/j3MAAOSwzLdk6Nx6/s-l225.webp",
     price: "299",
   },
   {
     id: "22",
-    description: "Nike Air Max 95 ",
+    description: "Nike Air Max 95",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/OWIAAOSwhdxk6NyC/s-l225.webp",
     price: "199",
   },
   {
     id: "23",
-    description:
-      "Nike Air Max 95 Essential Triple Black Running Sneakers Trainers Shoes Men Size",
+    description: "Nike Air Max 95 Essential",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/RpEAAOSwaABk6NyH/s-l225.webp",
     price: "199",
   },
   {
     id: "24",
-    description: "Nike React Pegasus Trail 4 GTX",
+    description: "Nike React Pegasus Trail",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/9HMAAOSw9KFk6NyN/s-l225.webp",
     price: "99",
   },
   {
     id: "25",
-    description: "Nike SB Dunk Low Size 11 ",
+    description: "Nike SB Dunk Low Size 11",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/LuEAAOSw2rpk6NyU/s-l225.webp",
     price: "129",
@@ -200,7 +189,7 @@ var productData = [
   },
   {
     id: "27",
-    description: "Nike Kobe 9 Bright Mango 2014",
+    description: "Nike Kobe 9 Bright",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/Ow8AAOSwqVBk6Nyk/s-l225.webp",
     price: "599",
@@ -214,7 +203,7 @@ var productData = [
   },
   {
     id: "29",
-    description: "Nike Dunk Low Pine Green Green-Varsity",
+    description: "Nike Dunk Low Pine Green",
     category: "nike",
     url: "https://i.ebayimg.com/thumbs/images/g/DaEAAOSwHO9k6Nyw/s-l225.webp",
     price: "249",
@@ -228,5 +217,82 @@ var productData = [
   },
 ];
 
-localStorage.setItem("productsData", JSON.stringify(productData));
-function loadProduct() {}
+localStorage.setItem("productData", JSON.stringify(productData));
+
+
+var productDiv1 = document.querySelector("#pd-1");
+function displayProduct(data) {
+  data.map(function (ele, index) {
+
+    var box = document.createElement("div");
+
+    var productImage = document.createElement("img");
+    productImage.setAttribute("src", ele.url);
+
+    var productDesc = document.createElement("h3");
+    productDesc.innerText = ele.description;
+
+    var productPrice = document.createElement("p");
+    productPrice.innerText = "₹ " + ele.price;
+
+    var addToCartBtn = document.createElement("button");
+
+    box.append(productImage, productDesc, productPrice, addToCartBtn);
+    productDiv1.append(box);
+    console.log(box);
+    addToCartBtn.innerText = "Add to Cart";
+    addToCartBtn.addEventListener("click", function () {
+      addToCart(ele);
+      console.log(data);
+    });
+  });
+}
+
+var data = JSON.parse(localStorage.getItem("productData")) || [];
+displayProduct(data);
+
+function sortByPrice(data) {
+  var value = document.querySelector("#sortByPrice").value;
+  if (value === "lth") {
+    data.sort(function (a, b) {
+      return a.price - b.price;
+    });
+  } else if (value === "htl") {
+    data.sort(function (a, b) {
+      return b.price - a.price;
+    })
+  } else {
+    window.location.href = "index.html";
+  }
+  // console.log(data)
+  displayProduct(data);
+}
+
+function filterByCategory(data) {
+  var value = document.querySelector("#fiterByCategory").value;
+
+  if (value === "trending") {
+    var filteredProduct = data.filter(function (ele) {
+      if (ele.category === "trending") {
+        return ele;
+      }
+    })
+    displayProduct(filteredProduct);
+  } else if (value === "adidas") {
+    var filteredProduct = data.filter(function (ele) {
+      if (ele.category === "adidas") {
+        return ele;
+      }
+    });
+    displayProduct(filteredProduct);
+  } else if (value === "nike") {
+    var filteredProduct = data.filter(function (ele) {
+      if (ele.category === "nike") {
+        return ele;
+      }
+    });
+    displayProduct(filteredProduct);
+  } else {
+    window.location.href = "index.html";
+  }
+}
